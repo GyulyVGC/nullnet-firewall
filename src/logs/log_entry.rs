b6d86f6aca5e53566d1095f5bc51c0fbd3be8955
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-11-22 14:49:23 +0100

    tests about LogEntry creation and display

--- src/logs/log_entry.rs
+++ src/logs/log_entry.rs
@@ -81,3 +81,119 @@ fn format_icmp_type(icmp_type: Option<u8>) -> String {
         "-".to_string()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::logs::log_ip::LogIp;
+    use crate::utils::raw_packets::test_packets::{ARP_PACKET, ICMPV6_PACKET, TCP_PACKET};
+    use crate::{Fields, FirewallAction, FirewallDirection, LogEntry};
+    use std::net::IpAddr;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_log_entry_new() {
+        // tcp packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&TCP_PACKET),
+            FirewallDirection::IN,
+            FirewallAction::DENY,
+        );
+        assert_eq!(log_entry_tcp.direction, FirewallDirection::IN);
+        assert_eq!(log_entry_tcp.action, FirewallAction::DENY);
+        assert_eq!(
+            log_entry_tcp.source,
+            Some(LogIp::from_ip_addr(Some(IpAddr::from_str("192.168.200.135").unwrap())).unwrap())
+        );
+        assert_eq!(
+            log_entry_tcp.dest,
+            Some(LogIp::from_ip_addr(Some(IpAddr::from_str("192.168.200.21").unwrap())).unwrap())
+        );
+        assert_eq!(log_entry_tcp.sport, Some(6711));
+        assert_eq!(log_entry_tcp.dport, Some(2000));
+        assert_eq!(log_entry_tcp.proto, Some(6));
+        assert_eq!(log_entry_tcp.icmp_type, None);
+        assert_eq!(log_entry_tcp.size, 66);
+
+        // icmpv6 packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&ICMPV6_PACKET),
+            FirewallDirection::OUT,
+            FirewallAction::REJECT,
+        );
+        assert_eq!(log_entry_tcp.direction, FirewallDirection::OUT);
+        assert_eq!(log_entry_tcp.action, FirewallAction::REJECT);
+        assert_eq!(
+            log_entry_tcp.source,
+            Some(
+                LogIp::from_ip_addr(Some(IpAddr::from_str("3ffe:501:4819::42").unwrap())).unwrap()
+            )
+        );
+        assert_eq!(
+            log_entry_tcp.dest,
+            Some(
+                LogIp::from_ip_addr(Some(
+                    IpAddr::from_str("3ffe:507:0:1:200:86ff:fe05:8da").unwrap()
+                ))
+                .unwrap()
+            )
+        );
+        assert_eq!(log_entry_tcp.sport, None);
+        assert_eq!(log_entry_tcp.dport, None);
+        assert_eq!(log_entry_tcp.proto, Some(58));
+        assert_eq!(log_entry_tcp.icmp_type, Some(135));
+        assert_eq!(log_entry_tcp.size, 86);
+
+        // arp packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&ARP_PACKET),
+            FirewallDirection::OUT,
+            FirewallAction::ACCEPT,
+        );
+        assert_eq!(log_entry_tcp.direction, FirewallDirection::OUT);
+        assert_eq!(log_entry_tcp.action, FirewallAction::ACCEPT);
+        assert_eq!(log_entry_tcp.source, None);
+        assert_eq!(log_entry_tcp.dest, None);
+        assert_eq!(log_entry_tcp.sport, None);
+        assert_eq!(log_entry_tcp.dport, None);
+        assert_eq!(log_entry_tcp.proto, None);
+        assert_eq!(log_entry_tcp.icmp_type, None);
+        assert_eq!(log_entry_tcp.size, 42);
+    }
+
+    #[test]
+    fn test_log_entry_display() {
+        // tcp packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&TCP_PACKET),
+            FirewallDirection::IN,
+            FirewallAction::DENY,
+        );
+        assert_eq!(
+            format!("{log_entry_tcp}")[34..].to_string(),
+            "IN DENY TCP 192.168.200.135 192.168.200.21 6711 2000 - 66".to_string()
+        );
+
+        // icmpv6 packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&ICMPV6_PACKET),
+            FirewallDirection::IN,
+            FirewallAction::ACCEPT,
+        );
+        assert_eq!(
+            format!("{log_entry_tcp}")[34..].to_string(),
+            "IN ACCEPT IPv6-ICMP 3ffe:501:4819::42 3ffe:507:0:1:200:86ff:fe05:8da - - 135 86"
+                .to_string()
+        );
+
+        // arp packet
+        let log_entry_tcp = LogEntry::new(
+            &Fields::new(&ARP_PACKET),
+            FirewallDirection::OUT,
+            FirewallAction::REJECT,
+        );
+        assert_eq!(
+            format!("{log_entry_tcp}")[34..].to_string(),
+            "OUT REJECT - - - - - - 42".to_string()
+        );
+    }
+}
